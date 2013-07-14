4517a4c083129fd44dc4c5e1ebcc4f9f8c6f1a73
amit chhajer <=>
2013-07-14 17:49:17 +0530

    read string from index in webjs

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(15);
+var string = fs.readFileSync('index.html', 'utf-8');
+
+buffer.write(string);
+var data = buffer.toString("utf-8", 0, 15);
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
